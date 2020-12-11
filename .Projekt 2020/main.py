f14e33f1480d6e9106a1affe91e6e0af7fb04e66
pdamaszk <pdamaszk@gmail.com>
2020-12-12 00:27:17 +0100

    wykres

--- .Projekt 2020/main.py
+++ .Projekt 2020/main.py
@@ -2,15 +2,115 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 from numpy import sin,cos
-#
 
+def double_pen_ani_wyk(L1, th1, M1, L2, th2, M2):
+    from numpy import sin, cos
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import scipy.integrate as integrate
+    import matplotlib.animation as animation
+
+    G = 9.8         # [m/s^2]       przyspieszenie ziemskie
+
+    # L1 = 0.3        # [m]           dlugosc pierwszego wahadla
+    # th1 = 90.0      # [stopnie]     kąt nachylenia pierwszego wahadla
+    # M1 = 100.0      # [kg]          masa pierwszego wahadla
+    #
+    # L2 = 1.0        # [m]           dlugosc drugiego wahadla
+    # th2 = -90.0     # [stopnie]     kąt nachylenia drugiego wahadla
+    # M2 = 2.0        # [kg]          masa drugiego wahadla
+
+    w1 = 0.0        # [m/s]         prędkość poczatkowa pierwszego wahadla
+    w2 = 0.0        # [m/s]         prędkość poczatkowa drugiego wahadla
+
+    dt = 0.05       # [s]           delta - dokładność obliczeń
+    t = np.arange(0, 60, dt)
+
+
+
+    def pochodna(state, t):
+
+        dydx = np.zeros_like(state)
+        dydx[0] = state[1]
+
+        delta = state[2] - state[0]
+        den1 = (M1+M2) * L1 - M2 * L1 * cos(delta) * cos(delta)
+        dydx[1] = ((M2 * L1 * state[1] * state[1] * sin(delta) * cos(delta)
+                    + M2 * G * sin(state[2]) * cos(delta)
+                    + M2 * L2 * state[3] * state[3] * sin(delta)
+                    - (M1+M2) * G * sin(state[0]))
+                   / den1)
+
+        dydx[2] = state[3]
+
+        den2 = (L2/L1) * den1
+        dydx[3] = ((- M2 * L2 * state[3] * state[3] * sin(delta) * cos(delta)
+                    + (M1+M2) * G * sin(state[0]) * cos(delta)
+                    - (M1+M2) * L1 * state[1] * state[1] * sin(delta)
+                    - (M1+M2) * G * sin(state[2]))
+                   / den2)
+
+        return dydx
+
+
+
+    # initial state
+    state = np.radians([th1, w1, th2, w2])
+
+    # integrate your ODE using scipy.integrate.
+    y = integrate.odeint(pochodna, state, t)
+
+    x1 = L1*sin(y[:, 0])
+    y1 = -L1*cos(y[:, 0])
+
+    x2 = L2*sin(y[:, 2]) + x1
+    y2 = -L2*cos(y[:, 2]) + y1
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, autoscale_on=False, xlim=(-1.1*(L1+L2), 1.1*(L1+L2)), ylim=(-1.1*(L1+L2), L1+0.5*L2))
+    # ax.set_ylim([-1.1*(L1+L2), 1.1*L1])
+    # ax.set_xlim([-1.1*(L1 + L2), 1.1*(L1+L2)])
+
+    ax.set_aspect('equal')
+    ax.grid()
+
+    line, = ax.plot([], [], 'o-', lw=2)
+    time_template = 'time = %.1fs'
+    time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+
+    def init():
+        line.set_data([], [])
+        time_text.set_text('')
+        return line, time_text
+
+
+    def animate(i):
+        thisx = [0, x1[i], x2[i]]
+        thisy = [0, y1[i], y2[i]]
+
+        line.set_data(thisx, thisy)
+        time_text.set_text(time_template % (i*dt))
+        return line, time_text
+
+
+    ani = animation.FuncAnimation(fig, animate, range(1, len(y)),
+                                  interval=dt*1000, blit=True, init_func=init)
+    plt.show()
+
+def rysuj():
+    double_pen_ani_wyk(float(Entry_L1.get()), float(Entry_Th1.get()), float(Entry_M1.get()), float(Entry_L2.get()), float(Entry_Th2.get()), float(Entry_M2.get()))
+
+# definicja icony okna
 def pull_icon(root,icon):
     root.iconbitmap(icon)
+# definicja srodek szerokosci okna canvas
 def canvas_X(x):
     return canvas_width // 2 + x
+# definicja srodek wysokosci okna canvas
 def canvas_Y(y):
     return canvas_height // 2 - y
-
+# definicja przetwarzania okna DODATKI
 def Calc_please(L1, Th1, L2, Th2):
     x1 = (L1 * sin(Th1))
     y1 = (-L1 * cos(Th1))
@@ -22,7 +122,7 @@ def Calc_please(L1, Th1, L2, Th2):
     W1.set(x1y1)
     W2.set(x2y2)
     draw_pendulum(x1, y1, x2, y2)
-
+# definicja rysowania wahadła <wizualizacja>
 def draw_pendulum(x1, y1, x2, y2):
     norm_x = (abs(x1) + abs(x2))/canvas_X(0)
     norm_y = (abs(y1) + abs(y2))/canvas_Y(0)
@@ -39,7 +139,6 @@ def draw_pendulum(x1, y1, x2, y2):
     y0 = canvas_Y(0)
     x1 /= norm
     x1 = canvas_X(int(x1))
-    print(x1)
     y1 /= norm
     y1 = canvas_Y(int(y1))
     x2 /= norm
@@ -59,11 +158,13 @@ def draw_pendulum(x1, y1, x2, y2):
     create_circle(C,x1, y1, 5, fill="blue", outline="black", width=2)
     create_circle(C,x2, y2, 5, fill="red", outline="black", width=2)
 
+# rysowanie okręgu w oknie <canvas>
 def create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
+# definicja reakcji na klawisz enter
 def eventEnter(event):
-    print(event.keysym)
+    # print(event.keysym)
     if event.keysym == "Return":
         checkvariables()
     # try:
@@ -72,8 +173,8 @@ def eventEnter(event):
     # except:
     #     pass
 
+# definicja sprawdzenie danych w oknie wahadel
 def checkvariables():
-
     try:
         L1 = float(Entry_L1.get())
     except:
@@ -124,11 +225,11 @@ def checkvariables():
 
 
 
-# testowanie zawartosci okna
+# definicja trybu testowego - testowanie zawartosci okna
 def test_interfecu():
     global test_id
     test_id = 1
-# configuracja testowania framów interfacu
+# configuracja - parametry trybu testowego
 test_colours = { 'title':(None,'yellow'),
               'left_frame':(None,'green'),
               'right_frame':(None,'orange'),
@@ -145,18 +246,18 @@ keypressed = False
 window_ico = 'ico2_32_32_2.ico'
 # tytuł okna
 window_title = " Wahadło podwójne - v1.0 Beta"
-
+# Czcionki
 global_font = "Calibri"
 main_font = f"{global_font} 12 "
 sub_font = f"{global_font} 10 italic"
 title_font = f"{global_font} 20 bold italic"
-
+# pełny ekran < wysokosc / szerokosc >
 window_width_change = False
 window_height_change = False
 
 padx_label = 8
 align_entry = 'right'
-
+#
 color_list = (None, 'white', 'red')
 bg_canvas = color_list[1]
 bg_button = color_list[0]
@@ -202,21 +303,16 @@ Labelframe_visualisation = tk.LabelFrame(Frame_visualisation,
 Labelframe_visualisation.grid(row=0, column=0, sticky='w')
 
 C = tk.Canvas(Labelframe_visualisation, height=canvas_height, width=canvas_width, bg=bg_canvas)
-
 C.pack()
-coord = 10, 50, 240, 210
-# arc = C.create_arc(coord, start=30, extent=210, fill="red")
 
 ## Frame LEWY
 frame_left = tk.Frame(Window_wahadlo, padx=5, pady=5, bg=test_colours['left_frame'][test_id])
 frame_left.grid(row=1, column=0, sticky='wn')
 
-##  (frama lewego)
+##  (frame lewy)
 ## ----- # górny frame "Górne wahadlo"
 Left_Frame = tk.LabelFrame(frame_left, padx=5, pady=5, text="Górne wahadlo", font=sub_font)
 Left_Frame.grid(row=0, column=0)
-
-
 
 label_L1 = tk.Label(Left_Frame, text=f"Długość wahadła L\u2081", padx=padx_label)
 label_Th1 = tk.Label(Left_Frame, text=f"Kąt {chr(952)}\u2081", padx=padx_label)
@@ -233,10 +329,10 @@ Entry_L1.grid(row=0, column=1)
 Entry_Th1.grid(row=1, column=1)
 Entry_M1.grid(row=2, column=1)
 
-
 ## ----- # dolny frame 1 "Dolne wahadlo"
 Left_Frame_2 = tk.LabelFrame(frame_left, padx=5, pady=5, text="Dolne wahadlo", font=sub_font)
 Left_Frame_2.grid(row=1, column=0)
+
 label_L2 = tk.Label(Left_Frame_2, text=f"Długość wahadła L\u2082", padx=padx_label)
 label_Th2 = tk.Label(Left_Frame_2, text=f"Kąt {chr(952)}\u2082", padx=padx_label)
 label_M2 = tk.Label(Left_Frame_2, text="Masa M\u2082", padx=padx_label)
@@ -244,7 +340,6 @@ label_M2 = tk.Label(Left_Frame_2, text="Masa M\u2082", padx=padx_label)
 Entry_L2 = tk.Entry(Left_Frame_2, borderwidth=2, width=10)
 Entry_Th2 = tk.Entry(Left_Frame_2, borderwidth=2, width=10)
 Entry_M2 = tk.Entry(Left_Frame_2, borderwidth=2, width=10)
-
 
 label_L2.grid(row=0, column=0, sticky='e')
 label_Th2.grid(row=1, column=0, sticky='e')
@@ -256,16 +351,18 @@ Entry_M2.grid(row=2, column=1)
 # ----- # dolny frame 2 "dodatki 1"
 Right_Frame = tk.Frame(frame_left, padx=5, pady=5, width=350)
 Right_Frame.grid(row=2, column=0)
+
 W1 = tk.StringVar()
 W2 = tk.StringVar()
-
 
 label_W0 = tk.Label(Right_Frame, text=f"(x\u2080, y\u2080) = ", width=15, anchor='e')
 label_W1 = tk.Label(Right_Frame, text=f"(x\u2081, y\u2081) = ", width=15, anchor='e')
 label_W2 = tk.Label(Right_Frame, text=f"(x\u2082, y\u2082) = ", width=15, anchor='e')
+
 label_W01 = tk.Label(Right_Frame, text="(0, 0)", width=10, anchor='w')
 label_W11 = tk.Label(Right_Frame, textvariable=W1, width=10, anchor='w')
 label_W21 = tk.Label(Right_Frame, textvariable=W2, width=10, anchor='w')
+
 Button_calc_coord = tk.Button(Right_Frame,
                               text= "Pokaż podgląd",
                               command=checkvariables,
@@ -275,29 +372,20 @@ Button_calc_coord = tk.Button(Right_Frame,
                               borderwidth=4,
                               bg=bg_button)
 
-# Entry_L2 = tk.Entry(Right_Frame, borderwidth=2, width=10)
-# Entry_Th2 = tk.Entry(Right_Frame, borderwidth=2, width=10)
-# Entry_M2 = tk.Entry(Right_Frame, borderwidth=2, width=10)
-
 label_W0.grid(row=1, column=0, sticky='e')
 label_W1.grid(row=2, column=0, sticky='e')
 label_W2.grid(row=3, column=0, sticky='e')
 label_W01.grid(row=1, column=1, sticky='w')
 label_W11.grid(row=2, column=1, sticky='w')
 label_W21.grid(row=3, column=1, sticky='w')
-Button_calc_coord.grid(row=4, column=0, columnspan=2)
 
-# Entry_L2.grid(row=0, column=1)
-# Entry_Th2.grid(row=1, column=1)
-# Entry_M2.grid(row=2, column=1)
+Button_calc_coord.grid(row=4, column=0, columnspan=2)
 
 Window_Frame_bottom = tk.Frame(Window_wahadlo, padx=5, pady=5, bg=test_colours['botton'][test_id], width=700, height=200)
 Window_Frame_bottom.grid(row=2, column=0, columnspan=2, sticky='w')
-
+# Okno Notebook
 Bottom_Frame = tk.LabelFrame(Window_Frame_bottom, padx=5, pady=5, text="", font=sub_font)
 Bottom_Frame.grid(row=0, column=0)
-
-
 
 # Notebook
 nb = ttk.Notebook(Bottom_Frame)
@@ -307,19 +395,27 @@ nb.grid(row=0, column=0, columnspan=2, sticky='NEWS')
 f1 = tk.Frame(nb)
 f1_menu = tk.Label(f1, text="tu beda do wyboru wykresy")
 f1_menu.grid(row=0, column=0)
+f1_button = tk.Button(f1, text= "Wykres",
+                              command=rysuj,
+                              padx=10,
+                              pady=2,
+                              relief='raised',
+                              borderwidth=4,
+                              bg=bg_button)
+f1_button.grid(row=1, column=0)
 
-#Dodawanie pierwszej zakładki
+# Dodawanie pierwszej zakładki
 nb.add(f1, text="Wykresy")
 # Tworzenie drugiej zakładki
 f2 = tk.Frame(nb)
-#Dodawanie drugiej zakładki
+# Dodawanie drugiej zakładki
 nb.add(f2, text="Ustawienia")
 # Tworzenie trzeciej zakładki
 f3 = tk.Frame(nb)
 f1_menu3 = tk.Label(f3, text="zrób ikonke do naszego \nprogramu !!", font=title_font, bg='white', width=25, height=3)
 f1_menu3.grid(row=0, column=0)
 
-#Dodawanie trzeciej zakładki
+# Dodawanie trzeciej zakładki
 nb.add(f3, text="Jan Kurek")
 
 nb.select(f1)
@@ -328,7 +424,10 @@ nb.enable_traversal()
 
 # button_2 = tk.Button(sq_frame2)
 # button_2.grid(row=0, column=0, sticky='n')
+
+# kursor na pole Entry_L1
 Entry_L1.focus()
+# zbieranie danych o wcisnietym klawiszu
 Window_wahadlo.bind("<Key>", eventEnter)
 
 Window_wahadlo.mainloop()
